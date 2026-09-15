fc75629846b92059fd4b72d88043922b38edada0
Юлия <lelkovayul@yandex.ru>
2026-09-15 16:20:30 +0300

    задание 3

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -1,6 +1,6 @@
-price=float(input())
-discount=float(input())
-vat=float(input())
+price=float(input('price:'))
+discount=float(input('discount:'))
+vat=float(input('vat:'))
 base=price*(1-discount/100)
 vat_amount=base*(vat/100)
 total=base+vat_amount
