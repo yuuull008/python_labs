f3cefa0a0d5609a4b223ca25be5633fda83d022d
Юлия <lelkovayul@yandex.ru>
2026-09-21 19:15:10 +0300

    задание 6

--- src/lab01/06_participants_count.py
+++ src/lab01/06_participants_count.py
@@ -1,12 +1,12 @@
-p_count=int(input())
+p_count=int(input("in_1:"))
 och=0
 zaoch=0
 for i in range(p_count):
-    line=input()
+    line=input(f"in_{i+2}:")
     part_line=line.split()
     time=part_line[3]
     if time=='True':
         och+=1
     elif time=='False':
         zaoch+=1
-print(och,zaoch)
+print("out:",och,zaoch)
